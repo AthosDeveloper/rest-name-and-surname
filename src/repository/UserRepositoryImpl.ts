f3df185bc6cfba4efbdbe7918d7e34ace8da82d8
AthosDeveloper <athos.silva@zup.com.br>
2024-01-09 14:32:07 -0300

    UserRepositoryImpl created

---
+++ src/repository/UserRepositoryImpl.ts
@@ -0,0 +1,22 @@
+import { User, UserRepository } from "../model/User";
+export class UserRepositoryImpl implements UserRepository {
+    private users: User[] = [];
+    findAll(): User[] {
+        return this.users;
+    }
+    save(user: User): void {
+        this.users.push(user);
+    }
+    findById(id: string): User | null {
+        const user = this.users.find(user => user.id === id);
+        return user || null;
+    }
+    update(id: string, user: User): void {
+        const index = this.users.findIndex(user => user.id === id);
+        if (index >= 0) {
+            this.users[index] = user;
+        } else {
+            throw new Error(`user with ${id} not found`);
+        }
+    }
+}
