a4aeb72911f1fe41826bbe3ec64b632f64390c12
AthosDeveloper <athos.silva@zup.com.br>
2024-01-09 15:44:58 -0300

    rename app to index, routes and controller finished

---
+++ src/routs/User-routes.ts
@@ -0,0 +1,8 @@
+import { Express, request, response} from "express";
+import { UserController } from "../controller/User-Controller";
+export function userRoutes(app: Express, UserController: UserController): void{
+app.get("/users", (req, res) => UserController.getUsers(req, res));
+    app.post("/users", (req, res) => UserController.createUser(req, res));
+    app.get("/users/:id", (req, res) =>  UserController.getUserById(req, res));
+    app.put("/users/:id", (req, res) => UserController.updateUser(req, res));
+} 
