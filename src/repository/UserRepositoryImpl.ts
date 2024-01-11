aed036ef76c4764e6e2b640d4cf66a68cdd509ec
AthosDeveloper <athos.silva@zup.com.br>
2024-01-11 11:32:27 -0300

    uuid was configured

--- src/repository/UserRepositoryImpl.ts
+++ src/repository/UserRepositoryImpl.ts
@@ -1,23 +1,26 @@
-import { User} from "../model/User";
+import { uid } from "uid";
+import { User } from "../model/User";
 import { UserRepository } from "./UserRepository";
 export class UserRepositoryImpl implements UserRepository {
+
     private users: User[] = [];
     findAll(): User[] {
         return this.users;
     }
     save(user: User): void {
+        user.uid = uid();
         this.users.push(user);
     }
-    findById(id: string): User | null {
-        const user = this.users.find(user => user.id === id);
+    findById(uid: string): User | null {
+        const user = this.users.find(user => user.uid === uid);
         return user || null;
     }
-    update(id: string, user: User): void {
-        const index = this.users.findIndex(user => user.id === id);
+    update(uid: string, user: User): void {
+        const index = this.users.findIndex(user => user.uid === uid);
         if (index >= 0) {
             this.users[index] = user;
         } else {
-            throw new Error(`user with ${id} not found`);
+            throw new Error(`user with ${uid} not found`);
         }
     }
 }
